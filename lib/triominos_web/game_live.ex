aaee9fe848bbda7268278dbf57e834d21b30f430
Arjen <aratramba@gmail.com>
2024-01-10 16:46:28 +0100

    add move validation

--- lib/triominos_web/game_live.ex
+++ lib/triominos_web/game_live.ex
@@ -148,7 +148,7 @@ defmodule TriominosWeb.GameLive do
 
   def render(assigns) do
     ~H"""
-    <div class="grid grid-cols-5 h-screen overflow-hidden relative">
+    <div class="grid grid-cols-5 h-screen overflow-hidden relative select-none">
       <div id="dragger" class="absolute inset-0 z-50">
         <div
           :if={@dragging != nil}
@@ -162,9 +162,7 @@ defmodule TriominosWeb.GameLive do
       </div>
 
       <%!-- topbar --%>
-      <div class="absolute inset-x-0 top-0 h-16 border z-40 bg-darkgray">
-        topbar
-      </div>
+      <div class="absolute inset-x-0 top-0 h-16 border z-40 bg-darkgray"></div>
 
       <%!-- pool --%>
       <div class="absolute w-[50vh] h-[50vh] top-0 right-0 hover:h-[75vh] hover:w-[75vh] transition-all border rounded-full translate-x-1/2 -translate-y-1/2 flex items-end xoverflow-scroll z-50">
@@ -264,18 +262,40 @@ defmodule TriominosWeb.GameLive do
   end
 
   def handle_event("drag_end", %{"x" => x, "y" => y}, socket) do
-    IO.puts(to_string(x) <> " " <> to_string(y))
+    piece = socket.assigns.dragging
 
     # check if piece on top of another
     on_top = Enum.find(socket.assigns.board, fn p -> p.x == x and p.y == y end)
+    top_neighbour = Enum.find(socket.assigns.board, fn p -> p.x == x and p.y == y - 1 end)
+    bottom_neighbour = Enum.find(socket.assigns.board, fn p -> p.x == x and p.y == y + 1 end)
+    left_neighbour = Enum.find(socket.assigns.board, fn p -> p.x == x - 1 and p.y == y end)
+    right_neighbour = Enum.find(socket.assigns.board, fn p -> p.x == x + 1 and p.y == y end)
+
+    top_valid = validate_move("top", piece, top_neighbour)
+    bottom_valid = validate_move("bottom", piece, bottom_neighbour)
+    left_valid = validate_move("left", piece, left_neighbour)
+    right_valid = validate_move("right", piece, right_neighbour)
+
+    has_neighbours = top_neighbour || bottom_neighbour || left_neighbour || right_neighbour
+
+    neighbours_valid = top_valid && bottom_valid && left_valid && right_valid
+
+    if !has_neighbours do
+      IO.puts("rejected: no neighbours")
+    end
 
     if on_top != nil do
       IO.puts("rejected: on top of another piece")
     end
 
-    if !on_top do
-      piece = socket.assigns.dragging
+    if !neighbours_valid do
+      IO.puts("rejected: neighbours not valid")
+    end
 
+    valid =
+      !on_top && has_neighbours && neighbours_valid
+
+    if valid do
       # remove piece from hand
       hand = Enum.reject(socket.assigns.hand, fn p -> p.id == piece.id end)
 
@@ -295,5 +315,91 @@ defmodule TriominosWeb.GameLive do
     else
       {:noreply, assign(socket, dragging: nil)}
     end
+  end
+
+  def validate_move("top", _piece, nil) do
+    true
+  end
+
+  def validate_move("top", piece, neighbour) do
+    IO.inspect(piece.value)
+    IO.inspect(neighbour.value)
+
+    [a, b, _c, _d, _e, f] = piece.value
+    [a2, b2, c2, d2, e2, _f2] = neighbour.value
+
+    rotation_ok = (a == -1 && a2 != -1) || (b == -1 && b2 != -1)
+    match1 = f == e2 && f != -1
+    match2 = b == c2 && b != -1
+    match3 = a == d2 && a != -1
+    valid = rotation_ok && ((match1 && match2) || match3)
+
+    IO.puts("top ok: #{valid}")
+    valid
+  end
+
+  def validate_move("bottom", _piece, nil) do
+    true
+  end
+
+  def validate_move("bottom", piece, neighbour) do
+    IO.inspect(piece.value)
+    IO.inspect(neighbour.value)
+
+    [a, b, c, _d, e, _f] = piece.value
+    [a2, b2, _c2, _d2, _e2, f2] = neighbour.value
+
+    rotation_ok = (a == -1 && a2 != -1) || (b == -1 && b2 != -1)
+    match1 = e == f2 && e != -1
+    match2 = c == b2 && c != -1
+    match3 = a == a2 && a != -1
+    valid = rotation_ok && ((match1 && match2) || match3)
+
+    IO.puts("bottom ok: #{valid}")
+    valid
+  end
+
+  def validate_move("left", _piece, nil) do
+    true
+  end
+
+  def validate_move("left", piece, neighbour) do
+    IO.inspect(piece.value)
+    IO.inspect(neighbour.value)
+
+    [a, b, _c, d, e, f] = piece.value
+    [a2, b2, c2, d2, _e2, _f2] = neighbour.value
+
+    rotation_ok = (a == -1 && a2 != -1) || (b == -1 && b2 != -1)
+    match1 = a == b2 && a != -1
+    match2 = e == d2 && e != -1
+    match3 = f == a2 && f != -1
+    match4 = d == c2 && d != -1
+    valid = rotation_ok && ((match1 && match2) || (match3 && match4))
+
+    IO.puts("left ok: #{valid}")
+    valid
+  end
+
+  def validate_move("right", _piece, nil) do
+    true
+  end
+
+  def validate_move("right", piece, neighbour) do
+    IO.inspect(piece.value)
+    IO.inspect(neighbour.value)
+
+    [a, b, c, d, _e, _f] = piece.value
+    [a2, b2, _c2, d2, e2, f2] = neighbour.value
+
+    rotation_ok = (a == -1 && a2 != -1) || (b == -1 && b2 != -1)
+    match1 = b == a2 && b != -1
+    match2 = d == e2 && d != -1
+    match3 = a == f2 && a != -1
+    match4 = c == d2 && c != -1
+    valid = rotation_ok && ((match1 && match2) || (match3 && match4))
+
+    IO.puts("right ok: #{valid}")
+    valid
   end
 end
